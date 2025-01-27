6a17bf3bfd8b1df00d9b402ef776304d55d939f6
jeremias <juansebas7011@gmail.com>
2025-01-27 16:20:04 -0500

    el problema de mas de una hora era un pinche {

--- funciones.js
+++ funciones.js
@@ -1,13 +1,18 @@
  function calcularSuma( ){
-    var num1 = parseFloat(document.getElementById(Primer-numero).value );
-    var num2 = parseFloat(document.getElementById(Segundo-numero).value);
- }
+    console.log("function lladada")
+    let num1 = parseFloat(document.getElementById("primer-numero").value);
+    let num2 = parseFloat(document.getElementById("segundo-numero").value);
+ 
 
  if(isNaN(num1)|| isNaN(num2)){
     alert("hey eso no se puede sumar");
-    return;}
+    return;
+    }
 
-  var suma= num1 + num2 ;
+  let suma= num1 + num2 ;
 
 
-  ("resultado").textContent= "el resultado es"+ suma;
+  document.getElementById("resultado").textContent= "el resultado es"+ suma;
+    console.log(suma);
+
+}
