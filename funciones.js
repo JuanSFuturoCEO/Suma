10e62cf7a7eeb78c1f00e1a69ecd654d49a40f83
jeremias <juansebas7011@gmail.com>
2025-01-27 15:25:51 -0500

    falta el css

--- funciones.js
+++ funciones.js
@@ -2,3 +2,12 @@
     var num1 = parseFloat(document.getElementById(Primer-numero).value );
     var num2 = parseFloat(document.getElementById(Segundo-numero).value);
  }
+
+ if(isNaN(num1)|| isNaN(num2)){
+    alert("hey eso no se puede sumar");
+    return;}
+
+  var suma= num1 + num2 ;
+
+
+  document.getElementById("resultado").textContent= suma;
